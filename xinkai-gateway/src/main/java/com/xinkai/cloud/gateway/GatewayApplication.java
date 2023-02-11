de3da77dffa2048f8731e8b3e0883c657d574a6f
xinkai <1628535465@qq.com>
2023-02-12 00:07:46 +0800

    add xinkai-gateway project

---
+++ xinkai-gateway/src/main/java/com/xinkai/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.xinkai.cloud.gateway;
+
+/**
+ * @className: GatewayApplication
+ * @description: 应用启动类
+ * @author: xinkai
+ * @email: xinkai8011@gmail.com
+ * @date: 2023/2/11
+ **/
+
+public class GatewayApplication {
+    public static void main(String[] args) {
+
+    }
+}
