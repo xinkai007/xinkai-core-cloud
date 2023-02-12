00a46c2f870de863da08566b78715c31d56387b2
xinkai <1628535465@qq.com>
2023-02-12 11:55:44 +0800

    init xinkai-gateway info

--- xinkai-gateway/src/main/java/com/xinkai/cloud/gateway/GatewayApplication.java
+++ xinkai-gateway/src/main/java/com/xinkai/cloud/gateway/GatewayApplication.java
@@ -1,5 +1,9 @@
 package com.xinkai.cloud.gateway;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 /**
  * @className: GatewayApplication
  * @description: 应用启动类
@@ -7,9 +11,10 @@ package com.xinkai.cloud.gateway;
  * @email: xinkai8011@gmail.com
  * @date: 2023/2/11
  **/
-
+@SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(GatewayApplication.class, args);
     }
 }
